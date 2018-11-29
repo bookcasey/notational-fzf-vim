a997a6797f52e676e28e204374b173aebf1b9f91
Casey Olson <casey@bookcasey.com>
2018-11-28 19:33:48 -0600

    try extension again

--- shorten_path_for_notational_fzf.py
+++ shorten_path_for_notational_fzf.py
@@ -82,9 +82,9 @@ def process_line(line: str) -> str:
     #colored_short_name = ""
 
     # Quick and dirty extension removal, will not work for filenames than contain a '.'
-    simple_name = os.path.splitext(basename)[0]
+    # simple_name = os.path.splitext(basename)[0]
     
-    colored_short_name = color(simple_name, CYAN)
+    colored_short_name = color(basename, CYAN)
 
     # Format is: long form, line number, short form, line number, rest of line. This is so Vim can process it.
     formatted_line = ":".join(
